46ec4fc635591f9f6468725362803e49a222330e
Gilles Ooms <gillesooms@outlook.com>
2025-04-22 13:58:30 +0000

    Restructuration for second project

---
+++ file_system/skeleton/include/error.h
@@ -0,0 +1,10 @@
+#ifndef ERROR_H
+#define ERROR_H
+
+extern const int vdisk_ENODISK ;
+extern const int vdisk_EACCESS ;
+extern const int vdisk_ENOEXIST;
+extern const int vdisk_EEXCEED ;
+extern const int vdisk_ESECTOR ;
+
+#endif
---
+++ file_system/skeleton/main.c
@@ -0,0 +1,7 @@
+// Your main.c
+
+// if argc and argv not use, replace by "int main(void)" to suppress warnings at compilation
+int main(int argc, char *argv[]) {
+  
+    return 0;
+}
